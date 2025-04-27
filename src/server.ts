7ddbf4b50d43fe5dac9eb5fa61d7f54adb3fb129
Paulo Catto <paulohcatto@hotmail.com>
2025-04-27 20:48:57 -0300

    fix: update server to export handler for serverless deployment

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import app from "./app";
-import serverless from "serverless-http";
+import app from './app';
+import serverless from 'serverless-http';
 
 const handler = serverless(app);
 
 export default handler;
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production' && process.env.VERCEL !== '1') {
   const PORT = process.env.PORT || 3333;
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
-} else {
-  console.log("Rodando em produção - sem app.listen()");
 }
