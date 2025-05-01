861b3bf8ad8037b6a66106fd73394bc24ad70278
Paulo Catto <paulohcatto@hotmail.com>
2025-05-01 14:11:13 -0300

    feat: Set configs for deploy

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+const prisma = new PrismaClient({
+  log: ['warn', 'error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
 }
-
-const prisma = global.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
