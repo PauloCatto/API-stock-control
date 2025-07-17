957bbbde72e008fcbde85f0077cb9ac5b7987db8
Paulo Catto <paulohcatto@gmail.com>
2025-07-16 23:54:10 -0300

    refactor: optimize Prisma connection handling and simplify server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import app from './app';
+import prisma from './prisma';
 
-const port = process.env.PORT || 10000;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Prisma conectado!");
+    const port = process.env.PORT || 10000;
+    app.listen(port, () => {
+      console.log(`🟢 Servidor rodando na porta ${port}`);
+    });
+  } catch (error) {
+    console.error("Erro ao conectar Prisma:", error);
+    process.exit(1);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`🟢 Servidor rodando na porta ${port}`);
-});
-
+main();
